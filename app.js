2988d666f28c0e642c578cd618a4c2c8373c8d91
kdj9384 <kdj93843404@gmail.com>
2020-08-17 22:31:16 +0900

    only use in window

--- app.js
+++ app.js
@@ -3,43 +3,42 @@
 // __dirname : 현재까지의 주소
 // 즉 app.get() 은 비동기로 대기하고 있다가 해당하는 url로 접근이 확인되면 function을 실행시킨다.
 // GET: URL에 붙여서 전송 'naver.com/main.html' -> 길이제한, url에 정보 노출
-// POST: 
+// POST:
 
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var session = require('express-session')
-var flash = require('connect-flash')
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var session = require("express-session");
+var flash = require("connect-flash");
 
-var router = require('./router/index');
+var router = require("./router/index");
 
 // 서버 생성
 app.listen(3000, function () {
-    console.log("start express server on port 3000");
-})
+  console.log("start express server on port 3000");
+});
 
-console.log("end of server code...")
+console.log("end of server code...");
 // 동기화된 코드 먼저 실행, 후에 비동기 코드가 실행됨
 
-app.use(express.static('public')) // public 폴더를 express에 static으로 등록해서, 내용물을 쉽게 꺼내 쓸 수 있다.
-app.use(bodyParser.json()) // 이러한 형태의 문서를 bodyparser를 통해 처리하겠다.
-app.use(bodyParser.urlencoded({extended:true})) 
-app.set('view engine', 'ejs') // 뷰 엔진으로 ejs를 사용하겠다
-app.use(session({
-    secret:"#JDKLF439jsdlfsjl",
-    resave:false,
-    saveUninitialized:true
-}))
+app.use(express.static("public")); // public 폴더를 express에 static으로 등록해서, 내용물을 쉽게 꺼내 쓸 수 있다.
+app.use(bodyParser.json()); // 이러한 형태의 문서를 bodyparser를 통해 처리하겠다.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); // 뷰 엔진으로 ejs를 사용하겠다
+app.use(
+  session({
+    secret: "#JDKLF439jsdlfsjl",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(session({secret:"#JDKLF439jsdlfsjl"}))
+app.use(session({ secret: "#JDKLF439jsdlfsjl" }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
 app.use(router);
-
-
-
--- router/join/index.js
+++ router/join/index.js
@@ -1,58 +1,105 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path')
-var mysql = require('mysql');
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var session = require('express-session')
-var flash = require('connect-flash')
+var path = require("path");
+var mysql = require("mysql");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var session = require("express-session");
+var flash = require("connect-flash");
 
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    port : 3306,
-    database : 'jsman'
-})
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  port: 3306,
+  database: "jsman",
+});
 connection.connect();
 
-router.get('/', function(req, res) {
-    console.log("Join is loaded");
-    res.render('join.ejs');
-})
+router.get("/", function (req, res) {
+  var msg;
+  var errMsg = req.flash("error");
+  if (errMsg) msg = errMsg;
+  console.log("Join is loaded");
+  res.render("join.ejs", { message: msg });
+  // res.sendFile(path.join(__dirname, "../../public/join.html"));
+});
 
 // passport.use('local', new LocalStrategy({
 
 // }))
 
-passport.use('local-join', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
+passport.use(
+  "local-join",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
     },
-    function(req, email, password, done) {
-        console.log("local-join callback called")
+    function (req, email, password, done) {
+      console.log("local-join callback called");
+      var query = connection.query(
+        "select * from users where email=?",
+        [email],
+        function (err, rows) {
+          if (err) return done(err);
+          if (rows.length) {
+            console.log("existed user");
+            return done(null, false, { message: "your email is already used" });
+          } else {
+            var sql = { email: email, pw: password };
+            var query = connection.query(
+              "insert into user set ?",
+              sql,
+              function (err, rows) {
+                if (err) throw err;
+                return done(null, { email: email, uid: rows.insertId });
+              }
+            );
+          }
+        }
+      );
     }
-))
+  )
+);
 
-router.post('/', function(req, res) {
-    var email = req.body.email;
-    var name = req.body.name;
-    var passwd = req.body.password;
+router.post(
+  "/",
+  passport.authenticate("local-join", {
+    successRedirect: "/main",
+    failureRedirect: "/join",
+    failureFlash: true,
+  })
+);
 
-    // escaping query
-    
-    var sql = {email:email, name:name, pw:passwd};
-    var query = connection.query('insert into users set ?',sql, function(err, rows) {
-        if(err) {throw err;}
-        else res.render('welcom.ejs', {name:name, uid:rows.insertId});
-        
-        console.log("well", rows)
-    })
+// router.post("/", function (req, res) {
+//   var email = req.body.email;
+//   var name = req.body.name;
+//   var passwd = req.body.password;
 
-    // render는 views라는 파일을 자동으로 찾아준다.
-    // res.render(path.join(__dirname, '../../views/email.ejs'), {email : email})
-})
+//   // escaping query
 
+//   var sql = { email: email, name: name, pw: passwd };
+//   var query = connection.query("insert into users set ?", sql, function (
+//     err,
+//     rows
+//   ) {
+//     if (err) {
+//       throw err;
+//     } else
+//       res.render("welcom.ejs", {
+//         name: name,
+//         uid: rows.insertId,
+//         email: email,
+//         password: passwd,
+//       });
+
+//     console.log("well", rows);
+//   });
+
+//   // render는 views라는 파일을 자동으로 찾아준다.
+//   // res.render(path.join(__dirname, '../../views/email.ejs'), {email : email})
+// });
 
 module.exports = router;
